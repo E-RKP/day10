1f9bece99d2a87dedaae6f0d1fd491436c472696
E-RKP <edobackup111@gmail.com>
2025-05-06 16:52:47 +0200

    version 7

--- data.js
+++ data.js
@@ -1,2 +1,40 @@
 let prova = 'test'
 console.log(prova)
+let massimo = Number.NEGATIVE_INFINITY
+
+const arre = [[1,44,33,34,98,'abc'], ['ciao', 67]]
+
+for (i=0; i < arre.length ; i++){
+  for (j=0; j<arre[i].length; j++){
+     if (arre[i][j]> massimo && typeof arre[i][j] == typeof 1){
+
+        massimo = arre[i][j]  
+    } 
+  } 
+}
+
+console.log(massimo)
+
+
+const data = [[2, 99, 'a'], [-10, 33], ['b', 0]];
+// Output atteso: -10
+let min = Number.POSITIVE_INFINITY
+
+for (i=0; i<data.length; i++){
+  for (j=0; j<data[i].length; j++){
+
+    if (typeof data[i][j] == typeof 1 && data[i][j] < min){
+      min = data[i][j]
+    } 
+  }
+}
+
+console.log(min)
+
+
+
+
+
+
+
+
